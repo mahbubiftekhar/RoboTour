89cfbce214fb7c2ef3b28553849cd6860e88ec0e
FinnzC <chenzhan1997@gmail.com>
2018-02-01 11:38:34 +0000

    Optimal Path Finding in Progress. Dijsktra search algorithm not completed.

--- EV3/main.py
+++ EV3/main.py
@@ -33,42 +33,47 @@ def isThereObstacle():
         print("No obstacle detected.")
         return False
 
+def isKeepDistance():
+    if(abs(sonar.value() - obstacle_detection_distance) > 100):
+        return True
+    else:
+        return False
+
+
 def getArtPiecesFromApp(): # returns an list of direction commands
     #example
     pieces = ["Monalisa"]
     return pieces # returns list
 
-def moveForward():
-    motorLeft.run_timed(speed_sp=300, time_sp=100)
-    motorRight.run_timed(speed_sp=300, time_sp=100)
+def getCommandsFromServer():
+    commands = ["Speak", "Forward", "Goaround", "Stop"]
+    return commands
+
+def moveForward(speed, time):
+    motorLeft.run_timed(speed_sp=speed, time_sp=time)
+    motorRight.run_timed(speed_sp=speed, time_sp=time)
     #waitForMotor(motorLeft)                         # Can use something like this to prevent the program from progressing
     #waitForMotor(motorRight)                         # Can use something like this to prevent the program from progressing
     pass
 
-def moveBackward():
-    motorLeft.run_timed(speed_sp=-100, time_sp=100)
-    motorRight.run_timed(speed_sp=-100, time_sp=100)
-
+def moveBackward(speed, time):
+    motorLeft.run_timed(speed_sp=-speed, time_sp=time)
+    motorRight.run_timed(speed_sp=-speed, time_sp=time)
 
 def turnRight(degree):
     motorLeft.run_timed(speed_sp=100, time_sp = degree*10)
 
-
 def turnLeft(degree):
     motorRight.run_timed(speed_sp=100, time_sp=degree*10)
 
-def keepDistance():
-    if(abs(sonar.value() - obstacle_detection_distance) > 100):
-        moveBackward()
+def stopWheelMotor():
+    motorLeft.stop()
+    motorRight.stop()
 
-def waitForMotor(motor):
-    time.sleep(0.1)         # Make sure that motor has time to start
-    while motor.state==["running"]:
-        print('Motor is still running')
-        time.sleep(0.1)
+def speak(string):
+    ev3.Sound.speak(string).wait()
 
 def lineFinished():
-
     return False
 
 ##################### MAIN #################################
@@ -78,16 +83,48 @@ dictionary = {
     "Monalisa" : ["Forward", "Left", "Right"]
 }
 
-artPieces = getArtPiecesFromApp()
-commands = dictionary[artPieces[0]]
+#artPieces = getArtPiecesFromApp()
+#direction = dictionary[artPieces[0]]
+
+while (True):
+    commands = getCommandsFromServer()
+    i=0
+    if(commands[i] == "Forward"):
+        moveForward(300, 5000)
+    elif(commands[i] == "Left"):
+        turnLeft(10)
+    elif(commands[i] == "Right"):
+        turnRight(10)
+    elif(commands[i] == "Backward"):
+        moveBackward(300, 5000)
+    elif(commands[i] == "Speak"):
+        speak("Hello, I am RoboTour")
+    elif(commands[i] == "Goaround"):
+        while(commands[i+1] != None and commands[i+1] != "Stop"):
+            turnLeft(10)
+    elif(len(commands) == 0):
+        print("All commands finished")
+    else:
+        pass
+
+    i+=1
+
+"""
+
+def waitForMotor(motor):
+    time.sleep(0.1)         # Make sure that motor has time to start
+    while motor.state==["running"]:
+        print('Motor is still running')
+        time.sleep(0.1)
 
 for command in commands:
     if(command == "Forward"):
         while(not lineFinished()):
             if(not isThereObstacle()):
-                moveForward()
+                moveForward(300, 100)
             else:
-                keepDistance()
+                if(isKeepDistance()):
+                    moveBackward(100, 100)
     elif(command == "Left"):
         while(lineFinished()):
             turnLeft(10)
@@ -96,8 +133,8 @@ for command in commands:
             turnRight(10)
     else:
         pass
-
-"""
+        
+        
 while(1):
     if(not isThereObstacle()):
         moveForward()
