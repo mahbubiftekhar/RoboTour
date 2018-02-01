fe8efe481fc937feb22222ebbfbf60db4ede01ee
FinnzC <chenzhan1997@gmail.com>
2018-02-01 11:40:31 +0000

    Basic Commands Completed. Stop function needs to be tested with the motor and http://ev3dev-lang.readthedocs.io/projects/python-ev3dev/en/st

---
+++ EV3/tree_search.py
@@ -0,0 +1,8 @@
+def bfs(graph, start):
+    visited, queue = set(), [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
