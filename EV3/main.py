3c6d5e83ed7619811aadd6d523610bcf99069e4f
Alice Wu <32497595+AliceWoooo@users.noreply.github.com>
2018-02-04 20:18:12 +0000

    Edit Obstacle Avoidance
    1. Remove keep distance (Stop when detect obstacle)
    2. Stop only when going forward (Not stopping when turning or going backward)

--- EV3/main.py
+++ EV3/main.py
@@ -4,6 +4,8 @@ import time
 import ev3dev.ev3 as ev3
 from urllib.request import urlopen
 import re
+from threading import Thread
+
 
 
 ####################### GLOBAL VARIABLE ####################
@@ -20,34 +22,26 @@ currentcommandid = "0"
 sonar = ev3.UltrasonicSensor(ev3.INPUT_2)
 sonar.mode = 'US-DIST-CM' # Will return value in mm
 motorHand = ev3.LargeMotor('outB')
-motorLeft=ev3.LargeMotor('outA')
-motorRight=ev3.LargeMotor('outC')
+motorLeft = ev3.LargeMotor('outA')
+motorRight= ev3.LargeMotor('outC')
 
 if(motorHand.connected & sonar.connected & motorLeft.connected & motorRight.connected):
     print('All sensors and motors connected')
 else:
+    if(not motorHand.connected):
+        print("MotorHand not connected")
+    if(not sonar.connected):
+        print("Sonar not connected")
+    if(not motorLeft.connected):
+        print("MotorLeft not connected")
+    if(not motorRight.connected):
+        print("MotorRight not connected")
     print('Please check all sensors and actuators are connected.')
     exit()
 
 ############################################################
 
 ##################### FUNCTIONS ############################
-def isThereObstacle():
-    print(sonar.value())
-    if(sonar.value() < obstacle_detection_distance):
-        print("Obstacle detected.")
-        return True
-    else:
-        print("No obstacle detected.")
-        return False
-
-def isKeepDistance():
-    if(abs(sonar.value() - obstacle_detection_distance) > 100):
-        return True
-    else:
-        return False
-
-
 def getArtPiecesFromApp(): # returns an list of direction commands
     #example
     pieces = ["Monalisa"]
@@ -56,7 +50,9 @@ def getArtPiecesFromApp(): # returns an list of direction commands
 def getCommandFromServer():
     global currentcommandid
     global previouscommandid
+
     f = urlopen(link) #open url
+
     myfile = f.read() #read url contents
     string = myfile.decode("utf-8") #convert bytearray to string
     array = re.split('-', string)
@@ -78,14 +74,15 @@ def moveBackward(speed, time):
     motorRight.run_timed(speed_sp=-speed, time_sp=time)
 
 def turnRight(degree):
-    motorLeft.run_timed(speed_sp=100, time_sp = degree*10)
-    motorRight.run_timed(speed_sp=-100, time_sp = degree*10)
+    motorLeft.run_timed(speed_sp=200, time_sp = degree*10)
+    motorRight.run_timed(speed_sp=-200, time_sp = degree*10)
 
 def turnLeft(degree):
-    motorRight.run_timed(speed_sp=100, time_sp=degree*10)
-    motorLeft.run_timed(speed_sp=-100, time_sp=degree*10)
+    motorRight.run_timed(speed_sp=300, time_sp=degree*10)
+    motorLeft.run_timed(speed_sp=-300, time_sp=degree*10)
 
 def stopWheelMotor():
+    print(sonar.value())
     motorLeft.stop()
     motorRight.stop()
 
@@ -95,9 +92,58 @@ def speak(string):
 def lineFinished():
     return False
 
+def onPauseCommand():
+    pass
+
+def onResumeCommand():
+    pass
+
+##################### Multithreading #######################
+def isThereObstacle():
+    #print(sonar.value())
+    if(sonar.value() < obstacle_detection_distance):
+        #print("Obstacle detected.")
+        #speak("There is an obstacle detected.")
+        return True
+    else:
+        #print("No obstacle detected.")
+        return False
+"""
+def keepDistance():
+    if(abs(sonar.value() - obstacle_detection_distance) > 100):
+        moveBackward(100,100)
+
+class ObstacleAvoidanceThread(Thread):
+    def __init__(self):
+        ''' Constructor. '''
+        Thread.__init__(self)
+
+    def run(self):
+        while(True):
+            if(isThereObstacle()):
+                keepDistance()
+"""
+
+def obstacleAvoidance():
+    while(True):
+        if(command=="FORWARD"):
+            if(isThereObstacle()):
+                stopWheelMotor()
+
+
+############################################################
+
+speak("Carson, we love you. Group 18. ")
+
+"""
+obstacleAvoidanceThread = ObstacleAvoidanceThread()
+obstacleAvoidanceThread.setName("ObstacleAvoidanceThread")
+obstacleAvoidanceThread.start()
+"""
+
+t = Thread(target=obstacleAvoidance)
+t.start()
 ##################### MAIN #################################
-
-
 dictionary = {
     "Monalisa" : ["Forward", "Left", "Right"]
 }
@@ -116,18 +162,19 @@ while (True):
         if(command == "STOP"):
             stopWheelMotor()
         elif(command == "FORWARD"):
-            moveForward(300, 5000)
+            moveForward(300, 10000)
         elif(command == "BACKWARDS"):
             moveBackward(300, 5000)
         elif(command == "SPEAK"):
             speak("Hello, I am RoboTour")
-        elif(command == "GOAROUND"):
-                turnLeft(10000)
+        elif(command == "RIGHT"):
+            turnRight(10000)
+        elif(command == "LEFT"):
+            turnLeft(10000)
         else:
             pass
 
 """
-
 def waitForMotor(motor):
     time.sleep(0.1)         # Make sure that motor has time to start
     while motor.state==["running"]:
@@ -151,10 +198,9 @@ for command in commands:
     else:
         pass
 
-
 while(1):
     if(not isThereObstacle()):
-        moveForward()
+        moveForward(300, 100)
     else:
         keepDistance()
 
