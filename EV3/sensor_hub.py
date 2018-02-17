820138d2fa5c31efe18135f5ce56b0df6132010a
Michal Dauenhauer <michuszkud@gmail.com>
2018-02-17 19:21:32 +0000

    Finish prototype communication with ev3

--- EV3/sensor_hub.py
+++ EV3/sensor_hub.py
@@ -2,25 +2,61 @@
 
 import serial
 
-ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
+#ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
 
+class SensorHub():
 
+	def __init__(self):
+		self.baud = 9600
+		self.serial_port = serial.Serial(port='/dev/ttyACM0',\
+										 baudrate=self.baud)
+		self.n_sonars = 4
+		self.sonar_maxrange = 255
 
-def Sonar_read():
+		self.poll_timeout = 100
 
-	out =' '
-	while out[-1] != '\n':
-		while ser.inWaiting() > 0:
-			out += ser.read(1).decode('ascii')
+		self.__DEBUG__ = False
 
-	if(out != ''):
-		reads = out.split(',')
+		self.sensor_values = {}
 
-		for r in reads:
-			data = r.split(":")
-			print(data)
-			#print("Sensor {} : {}".format(data[0], data[1]))
+	def poll(self):
+		self.send_request()
+		frame = self.get_frame()
+		self.extract_from_frame(frame)
+
+	def send_request(self):
+		self.serial_port.write(b'r\n')
+		if self.__DEBUG__:
+			print("sending request")
+
+	def get_frame(self):
+		out = ''
+		while self.serial_port.inWaiting() > 0:
+			out += self.serial_port.read(1).decode('ascii')
+			if(out[-1] == '\n'):
+				out = out[:-2]
+				break
+
+		return out if out != '' else None
+
+	def extract_from_frame(self, frame):
+		if frame is None:
+			return
+		try:
+
+			readings = frame.split(',')
+			for r in readings:
+				data = r.split(':')
+				
+				self.sensor_values[data[0]] = data[1]
+				if self.__DEBUG__:
+					print("Sensor {}: {}cm".format(data[0], data[1]))
+		except:
+			print("Error processing frame: {}".format(frame))
+		
+
 
 if __name__ == "__main__":
+	sh = SensorHub()
 	while True:
-		Sonar_read()
+		sh.poll()
--- arduino/sonar_serial/sonar_serial.ino
+++ arduino/sonar_serial/sonar_serial.ino
@@ -9,6 +9,7 @@
 
 uint16_t sonar_val[SONAR_NUM];
 
+
 NewPing sonar[SONAR_NUM] = {
   NewPing(3, A3, MAX_DISTANCE),   //S0 - bottom most w coil facing up
   NewPing(5, A2, MAX_DISTANCE)    //S1 
@@ -16,24 +17,52 @@ NewPing sonar[SONAR_NUM] = {
   //NewPing(6, A0, MAX_DISTANCE)  //S3 - top most (PCB centre)
 };
 
-void setup() {
-  
+String inputString = "";
+bool newCommand = false;
 
+void setup() {
   Serial.begin(BAUD);
+
+  inputString.reserve(16);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  for(uint8_t i = 0; i < SONAR_NUM; ++i) {
+    //delay(PING_DELAY);  
+    sonar_val[i] = i;// sonar[i].ping_cm();
+  }
+
+  if(newCommand == true) {
+    //Serial.print(inputString);
+    sendSensorData();
+    newCommand = false;
+    inputString = "";
+  }
+  
+  
+ 
+}
+
+void serialEvent() {
+  while(Serial.available()) {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if(inChar == '\n') {
+      newCommand = true;
+    }
+  }
+}
+
+void sendSensorData() {
 
   for(uint8_t i = 0; i < SONAR_NUM; ++i) {
-    delay(PING_DELAY);  
-    sonar_val[i] = sonar[i].ping_cm();
     Serial.print(i);
     Serial.print(':');
     Serial.print(sonar_val[i]);
     Serial.print(',');
-    
   }
-  Serial.println();
-  
+  Serial.print('\n');
 }
+
