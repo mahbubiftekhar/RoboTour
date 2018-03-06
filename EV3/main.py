f9daabfd4508a8c6118b223b078cf7c42533d944
AliceWoooo <alicewoo358@gmail.com>
2018-03-06 13:50:59 +0000

    Edit Map

--- EV3/main.py
+++ EV3/main.py
@@ -37,7 +37,7 @@ def initialising_map():
     orientation_map[('0', '8')] = "N"
     orientation_map[('1', '12')] = "S"
     orientation_map[('1', '0')] = "N"
-    orientation_map[('2', '9')] = "N"
+    orientation_map[('2', '15')] = "N"
     orientation_map[('2', '3')] = "S"
     orientation_map[('3', '2')] = "N"
     orientation_map[('3', '13')] = "S"
@@ -47,14 +47,14 @@ def initialising_map():
     orientation_map[('5', '6')] = "E"
     orientation_map[('6', '5')] = "W"
     orientation_map[('6', '7')] = "E"
-    orientation_map[('7', '15')] = "ES"
+    orientation_map[('7', '9')] = "ES"
     orientation_map[('7', '6')] = "W"
     orientation_map[('8', '0')] = "S"
-    orientation_map[('8', '9')] = "E"
+    orientation_map[('8', '15')] = "E"
     orientation_map[('8', '14')] = "W"
-    orientation_map[('9', '15')] = "E"
-    orientation_map[('9', '2')] = "S"
-    orientation_map[('9', '8')] = "W"
+    orientation_map[('9', '13')] = "SW"
+    orientation_map[('9', '15')] = "W"
+    orientation_map[('9', '7')] = "NW"
     orientation_map[('10', '11')] = "N"
     orientation_map[('11', '10')] = "S"
     orientation_map[('11', '4')] = "N"
@@ -63,34 +63,35 @@ def initialising_map():
     orientation_map[('12', '1')] = "N"
     orientation_map[('12', '11')] = "W"
     orientation_map[('13', '3')] = "N"
-    orientation_map[('13', '15')] = "EN"
+    orientation_map[('13', '9')] = "EN"
     orientation_map[('13', '12')] = "W"
     orientation_map[('14', '4')] = "S"
     orientation_map[('14', '8')] = "E"
     orientation_map[('14', '5')] = "NE"
-    orientation_map[('15', '13')] = "SW"
-    orientation_map[('15', '9')] = "W"
-    orientation_map[('15', '7')] = "NW"
+    orientation_map[('15', '9')] = "E"
+    orientation_map[('15', '2')] = "S"
+    orientation_map[('15', '8')] = "W"
 
     # Distance Map for Dijkstra
     global dijkstra_map
     dijkstra_map = {
         '0': {'1': 26, '8': 21},
         '1': {'0': 26, '12': 19.5},
-        '2': {'3': 26.5, '9': 19.5},
+        '2': {'3': 26.5, '15': 19.5},
         '3': {'2': 26.5, '13': 20},
         '4': {'11': 33.5, '14': 31.5},
         '5': {'6': 27, '14': 46},
         '6': {'5': 27, '7': 28},
-        '7': {'6': 28, '15': 46.5},
-        '8': {'0': 21, '9': 31.5, '14': 28},
-        '9': {'2': 19.5, '8': 31.5, '15': 32},
+        '7': {'6': 28, '9': 46.5},
+        '8': {'0': 21, '15': 31.5, '14': 28},
+        '9': {'7': 46.5, '15': 32, '13': 85},
         '10': {'11': 20},
         '11': {'4': 33.5, '10': 20, '12': 28},
         '12': {'1': 19.5, '11': 28, '13': 32},
-        '13': {'3': 20, '12': 32, '15': 85},
+        '13': {'3': 20, '12': 32, '9': 85},
         '14': {'4': 31.5, '5': 46, '8': 28},
-        '15': {'7': 46.5, '9': 32, '13': 85}
+        '15': {'2': 19.5, '8': 31.5, '9': 32}
+
     }
 
     global motor_map
@@ -104,7 +105,7 @@ def initialising_map():
         '6': "N",
         '7': "N",
         '8': "N",
-        '9': "N"
+        '9': "E"
     }
 
 #####################################################################
