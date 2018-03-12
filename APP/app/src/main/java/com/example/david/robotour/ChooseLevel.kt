0789ac49195ef9c1334c1ba7caab6d0b54cc1482
mahbubiftekhar <mahbub@iftekhar.co.uk>
2018-03-12 00:13:46 +0000

    minor change

--- APP/app/src/main/java/com/example/david/robotour/ChooseLevel.kt
+++ APP/app/src/main/java/com/example/david/robotour/ChooseLevel.kt
@@ -1,7 +1,6 @@
 package com.example.david.robotour
 
 import android.annotation.SuppressLint
-import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
